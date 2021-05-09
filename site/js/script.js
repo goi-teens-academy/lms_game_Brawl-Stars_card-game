8db296287d293d495560bdd11fee605f1e592833
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-05-09 18:37:06 +0300

    add music

--- site/js/script.js
+++ site/js/script.js
@@ -12,6 +12,7 @@ refs.gameType = "singlePlayer";
 document.querySelector(".start__btn").addEventListener("click", () => {
   document.querySelector(".settings").classList.remove("hidden-modal");
   document.querySelector(".start").classList.add("hidden-modal");
+  document.querySelector(".audio__main-theme").play();
 });
 
 document
@@ -23,37 +24,52 @@ document.querySelectorAll(".settings__type").forEach((type) => {
   type.addEventListener("click", () => {
     refs.gameType = event.target.parentNode.dataset.type;
     document
-        .querySelectorAll(".settings__by-type")
-        .forEach((settings) => settings.classList.add("hidden-modal"));
-      document
-        .querySelector(`.settings__${event.currentTarget.dataset.type}`)
-        .classList.remove("hidden-modal");
+      .querySelectorAll(".settings__by-type")
+      .forEach((settings) => settings.classList.add("hidden-modal"));
     document
-        .querySelector(".settings__type--active")
-        .classList.remove("settings__type--active");
-      event.currentTarget.classList.add("settings__type--active");
+      .querySelector(`.settings__${event.currentTarget.dataset.type}`)
+      .classList.remove("hidden-modal");
+    document
+      .querySelector(".settings__type--active")
+      .classList.remove("settings__type--active");
+    event.currentTarget.classList.add("settings__type--active");
+  });
+});
+document.querySelectorAll(".count-cards__wrapper").forEach((container) => {
+  container.addEventListener("click", () => {
+    refs.cardsAmount = +event.target.dataset.count;
+    event.target.parentNode
+      .querySelector(".settings__btn--choosed")
+      .classList.remove("settings__btn--choosed");
+    event.target.classList.add("settings__btn--choosed");
   });
 });
 document
-  .querySelector(".count-cards__wrapper")
-  .addEventListener(
-    "click",
-    () => (refs.cardsAmount = +event.target.dataset.count)
-  );
-document
   .querySelector(".count-timer__wrapper")
-  .addEventListener(
-    "click",
-    () => (refs.timerCount = +event.target.dataset.count)
-  );
-document.querySelector(".settings__start-btn").addEventListener("click", function () {
-  startGame(
-    refs.cardsAmount,
-    cards,
-    refs.containerRef,
-    refs.timerCount,
-    refs.playerAmount,
-    refs.gameType
-  );
-  refs.settingsRef.classList.add("hidden");
-});
+  .addEventListener("click", () => {
+    refs.timerCount = +event.target.dataset.count;
+    event.target.parentNode
+      .querySelector(".settings__btn--choosed")
+      .classList.remove("settings__btn--choosed");
+    event.target.classList.add("settings__btn--choosed");
+  });
+  document.querySelector(".count-player__wrapper").addEventListener("click", () => {
+    refs.playerAmount = +event.target.dataset.count;
+    event.target.parentNode
+      .querySelector(".settings__btn--choosed")
+      .classList.remove("settings__btn--choosed");
+    event.target.classList.add("settings__btn--choosed");
+  });
+document
+  .querySelector(".settings__start-btn")
+  .addEventListener("click", function () {
+    startGame(
+      refs.cardsAmount,
+      cards,
+      refs.containerRef,
+      refs.timerCount,
+      refs.playerAmount,
+      refs.gameType
+    );
+    refs.settingsRef.classList.add("hidden");
+  });
